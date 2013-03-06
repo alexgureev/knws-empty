2bdb02982a548fcf2a7437fa036ac9532a32c089
barif <w3db@yandex.ru>
2013-03-06 23:52:32 +0200

    prepare to developer branch

---
+++ application/controllers/Frontpage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Description of Frontpage
+ * @see http://knws.ru/docs/Frontpage Documentation of Knws\Frontpage.
+ * @author Barif
+ */
+
+namespace Knws;
+
+use Knws\Controller;
+
+class Frontpage extends Controller
+{
+    
+}
