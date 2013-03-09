b2229e4e14b0db7c8c36383f3753825e090fab1c
barif <w3db@yandex.ru>
2013-03-09 23:38:11 +0200

    AppKernel class, console

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -10,3 +10,19 @@ if (!class_exists("Doctrine\Common\Version", false)) {
 }
 
 require_once "repositories/BugRepository.php";
+
+/*
+ *
+require_once __DIR__.'/../app/AppKernel.php';
+//require_once __DIR__.'/../app/AppCache.php';
+
+$kernel = new AppKernel('prod', false);
+$kernel->loadClassCache();
+//$kernel = new AppCache($kernel);
+Request::enableHttpMethodParameterOverride();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
+
+ */
